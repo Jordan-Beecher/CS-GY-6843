2f1a385d1ac857eaab1d2e95225b983985785040
Jordan Beecher <jab10032@nyu.edu>
2026-06-12 17:41:18 -0400

    Add webServer.py to Python-Programming-Web-Server branch

---
+++ webServer.py
@@ -0,0 +1,50 @@
+#CS-GY 6843 - Computer Networking
+#Name - Jordan Beecher
+#UNI - jab10032
+#NYU Email - jab10032@nyu.edu 
+
+from socket import *
+import sys
+
+def webServer(port=13331):
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  serverSocket.bind(("", port))
+  serverSocket.listen()
+  
+  while True:
+    
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    
+    try:
+      message = connectionSocket.recv(1024)
+      filename = message.split()[1]
+      f = open(filename[1:], 'rb')
+      
+      
+      outputdata = b"HTTP/1.1 200 OK\r\n"
+      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata += b"\r\n"
+
+               
+      for i in f: 
+        outputdata += i
+        connectionSocket.sendall(outputdata)
+      
+      connectionSocket.close() 
+      
+    except Exception as e:
+      outputdata = b"HTTP/1.1 404 Not Found\r\n"
+      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata += b"\r\n"
+      connectionSocket.sendall(outputdata)
+      connectionSocket.close()
+      
+
+  # Commenting out the below (some use it for local testing). It is not required for Gradescope, and some students have moved it erroneously in the While loop. 
+  # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
+  #serverSocket.close()
+  #sys.exit()  # Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+  webServer(13331)
