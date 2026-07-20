4d3d833623913b2044c6340f746759dd1d3ca4fd
Jordan Beecher <jab10032@nyu.edu>
2026-07-19 20:04:04 -0400

    subnets lab attempt 2

---
+++ CS-GY-6843/subnets.py
@@ -0,0 +1,81 @@
+#CS-GY 6843 - Computer Networking
+#Name - Jordan Beecher
+#UNI - jab10032
+#NYU Email - jab10032@nyu.edu 
+
+
+#enter your unique code
+uniquecode = 29000
+
+#problem 1
+p1Network = '248.174.160.192'
+p1Broadcast = '248.174.160.255'
+p1First = '248.174.160.193'
+p1Last =  '248.174.160.254'
+
+#problem 2
+p2Network = '159.159.122.0'
+p2Broadcast = '159.159.122.255'
+p2First = '159.159.122.1'
+p2Last = '159.159.122.254'
+
+#problem 3
+p3Network = '70.144.84.0'
+p3Broadcast = '70.144.84.31'
+p3First = '70.144.84.1'
+p3Last = '70.144.84.30'
+
+#problem 4
+p4Network = '237.129.45.0'
+p4Broadcast = '237.129.45.127'
+p4First = '237.129.45.1'
+p4Last = '237.129.45.126'
+
+#problem 5
+p5Network = '148.115.7.96'
+p5Broadcast = '148.115.7.111'
+p5First = '148.115.7.97'
+p5Last = '148.115.7.110'
+
+#problem 6
+p6Network = '59.100.225.128'
+p6Broadcast = '59.100.225.191'
+p6First = '59.100.225.129'
+p6Last =  '59.100.225.190'
+
+#problem 7
+p7Network = '226.85.187.192'
+p7Broadcast = '226.85.187.199'
+p7First = '226.85.187.193'
+p7Last = '226.85.187.198'
+
+#problem 8
+p8Network = '137.70.148.224'
+p8Broadcast = '137.70.148.255'
+p8First = '137.70.148.225'
+p8Last = '137.70.148.254'
+
+#problem 9
+p9Network = '48.55.110.0'
+p9Broadcast = '48.55.110.255'
+p9First = '48.55.110.1'
+p9Last = '48.55.110.254'
+
+#problem 10
+p10Network = '215.40.72.64'
+p10Broadcast = '215.40.72.95'
+p10First = '215.40.72.65'
+p10Last = '215.40.72.94'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
